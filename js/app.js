74421da615cb5baf776839a9bec30890973b5897
siamcse <siamcse01@gmail.com>
2022-09-12 08:26:15 +0600

    remove selected item edited

--- js/app.js
+++ js/app.js
@@ -26,9 +26,10 @@ $('btn-plus').addEventListener('click',function(){
         const stringiFied = JSON.stringify(ToDos);
         localStorage.setItem('ToDos',stringiFied);
     }
-    render();
+    displayFromLS();
 })
-const render = () =>{
+//display from localstorage
+const displayFromLS = () =>{
     const previousToDos = JSON.parse(localStorage.getItem('ToDos'));
     const ul = $('todo-container');
     ul.innerHTML = '';
@@ -36,18 +37,25 @@ const render = () =>{
         const li = document.createElement('li');
         li.className = ('list-decimal ml-2 p-1');
         li.innerHTML = `
-            ${todo.title}
+            <span>${todo.title}</span>
             <button onclick="removeItem()" class="bg-red-500 text-white px-4 rounded-xl hover:bg-red-900">Remove</button>
         `;
         ul.appendChild(li);
     })
 }
+//button romove all for clear all data from local storage
 $('btn-minus').addEventListener('click',function(){
     localStorage.removeItem('ToDos');
-    render();
+    displayFromLS();
 })
-const removeItem = (todo) =>{
-    console.log(event.target.parentNode);
-    event.target.parentNode.className = 'hidden';
+//remove selected item from local storage
+const removeItem = () =>{
+    const text = event.target.parentNode.children[0].innerText;
+    const todosString = localStorage.getItem('ToDos');
+    const todos = JSON.parse(todosString);
+    const remainingTodos = todos.filter(todo => todo.title !== text);
+    const todosStringified = JSON.stringify(remainingTodos);
+    localStorage.setItem('ToDos',todosStringified);
+    displayFromLS();
 }
-render();
+displayFromLS();
